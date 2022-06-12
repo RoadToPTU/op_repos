69bd30b9e05b0fde073b1eff44cccfc3bc09af6a
unknown <123>
2022-06-12 08:06:04 +0300

    1

---
+++ lab_1/lab_1/Calc.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace lab_1
+{
+    /// <summary>
+    /// Логика взаимодействия для DB.xaml
+    /// </summary>
+    /// 
+  
+    public partial class Calc : Window
+    {
+        string value = "0";
+        int sign;
+        public Calc()
+        {
+            InitializeComponent();
+            
+        }
+        private void ToHome(object sender, RoutedEventArgs e)
+        {
+            var mw = new MainWindow();
+            mw.Show();
+            Hide(); 
+        }
+
+
+        static string f_calc(string a, string b, int sign)
+        {
+            switch (sign)
+            {
+                case 1: return (double.Parse(a) + double.Parse(b)).ToString();
+                case 2: return (double.Parse(a) - double.Parse(b)).ToString();
+                case 3: return (double.Parse(a) * double.Parse(b)).ToString();
+                case 4: return (double.Parse(a) / double.Parse(b)).ToString();
+                default: return "!";
+            }
+
+
+        }
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            textBox.Text += "0";
+        }
+
+        
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = f_calc(value, textBox.Text, sign);
+        }
+
+       
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            textBox.Text += "1";
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            textBox.Text += "2";
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            textBox.Text += "3";
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            textBox.Text += "4";
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            textBox.Text += "5";
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            textBox.Text += "6";
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            textBox.Text += "7";
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+            textBox.Text += "8";        }
+
+        private void Button_Click_11(object sender, RoutedEventArgs e)
+        {
+            textBox.Text += "9";
+        }
+
+        private void Button_Click_12(object sender, RoutedEventArgs e)
+        {
+            value = textBox.Text;
+            textBox.Text = "";
+            sign = 4;
+        }
+
+        private void Button_Click_13(object sender, RoutedEventArgs e)
+        {
+            value = textBox.Text;
+            textBox.Text = "";
+            sign = 3;        }
+
+        private void Button_Click_14(object sender, RoutedEventArgs e)
+        {
+            value = textBox.Text;
+            textBox.Text = "";
+            sign = 2;
+        }
+
+        private void Button_Click_15(object sender, RoutedEventArgs e)
+        {
+            value = textBox.Text;
+            textBox.Text = "";
+            sign = 1;
+        }
+
+        private void Button_Click_16(object sender, RoutedEventArgs e)
+        {
+            textBox.Text += ",";
+        }
+
+        private void Button_Click_17(object sender, RoutedEventArgs e)
+        {
+            if(textBox.Text[0] == '-')
+            {
+                textBox.Text = textBox.Text.Substring(1);
+            }
+            else
+            {
+                textBox.Text = "-"+textBox.Text;
+            }
+            
+            
+        }
+
+        private void Button_Click_18(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = "";
+        }
+
+        private void Button_Click_19(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = textBox.Text.Substring(0,textBox.Text.Length-1);
+            
+            
+        }
+    }
+}
---
+++ lab_1/lab_1/DB.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace lab_1
+{
+    /// <summary>
+    /// Логика взаимодействия для DB.xaml
+    /// </summary>
+
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string About { get; set; }
+    }
+    public partial class DB : Window
+    {
+        public DB()
+        {
+            InitializeComponent();
+            File.Delete("DB.csv");
+        }
+
+        private void ToHome(object sender, RoutedEventArgs e)
+        {
+            var mw = new MainWindow();
+            mw.Show();
+            Hide(); 
+
+        }
+
+        private void Add(object sender, RoutedEventArgs e)
+        {
+
+            string path = "DB.csv";
+
+            
+            var student = new Student();
+            student.ID = int.Parse(id1.Text);
+            student.Name = name.Text;
+            student.About = comment.Text;
+            File.AppendAllText(path, String.Format("{0};{1};{2}\n", student.ID, student.Name, student.About));
+        }
+
+        private void Del(object sender, RoutedEventArgs e)
+        {
+            var students = new List<Student>();
+            var student = new Student();
+            string path = "DB.csv";
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                string line;
+                line = streamReader.ReadLine() ;
+                while (line != null)
+                {
+                    student = new Student();
+                    var vals = line.Split(';');
+                    student.ID = Convert.ToInt32(vals[0]);
+                    student.Name = vals[1];
+                    student.About = vals[2];
+                    if (student.ID != int.Parse(id2.Text))
+                    {
+                        students.Add(student);
+                    }
+                    line = streamReader.ReadLine() ;
+                }
+            }
+
+            File.Delete(path);
+
+          
+
+            foreach (var stud in students) 
+            {
+                File.AppendAllText(path, String.Format("{0};{1};{2}\n", stud.ID, stud.Name, stud.About));
+                
+            }
+            
+        }
+
+       
+    }
+}
